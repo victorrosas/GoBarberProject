085e9453dcfd4a34805f1edfb349d19237f5d42f
Victor Rosas <victor.rosas.96@hotmail.com>
2020-05-06 00:23:19 -0300

    Criando repositorio do TypeORM

--- src/database/migrations/1588730983242-CreateAppointments.ts
+++ src/database/migrations/1588730983242-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1588730983242
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
