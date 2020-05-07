bc261ed9f235a3359455747108c34d4646d5ba59
Victor Rosas <victor.rosas.96@hotmail.com>
2020-05-06 23:27:43 -0300

    Validando credenciais

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 
 import appointmentsRouter from './appointments.routes';
 import usersRouter from './users.routes';
+import sessionsRouter from './sessions.routes';
 
 const routes = express.Router();
 routes.use(express.json());
 
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
